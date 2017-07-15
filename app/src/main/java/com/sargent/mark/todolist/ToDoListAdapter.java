1c0478fdcdd206e6696001c408d6df3834b4112f
cowboyuniverse <cowboyuniverse@gmail.com>
2017-07-15 01:35:16 -0700

    updating project with marks code and added checkboxes

---
+++ app/src/main/java/com/sargent/mark/todolist/ToDoListAdapter.java
@@ -0,0 +1,119 @@
+package com.sargent.mark.todolist;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.sargent.mark.todolist.data.Contract;
+import com.sargent.mark.todolist.data.ToDoItem;
+
+import java.util.ArrayList;
+
+import static android.R.attr.checked;
+
+//import static com.sargent.mark.todolist.R.id.descriptions;
+
+/**
+ * Created by mark on 7/4/17.
+ */
+
+public class ToDoListAdapter extends RecyclerView.Adapter<ToDoListAdapter.ItemHolder> {
+
+    private Cursor cursor;
+    private ItemClickListener listener;
+    private String TAG = "todolistadapter";
+
+    @Override
+    public ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.item, parent, false);
+        ItemHolder holder = new ItemHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemHolder holder, int position) {
+        holder.bind(holder, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cursor.getCount();
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(int pos, String description, String duedate, long id);
+    }
+
+    public ToDoListAdapter(Cursor cursor, ItemClickListener listener) {
+        this.cursor = cursor;
+        this.listener = listener;
+    }
+
+    public void swapCursor(Cursor newCursor){
+        if (cursor != null) cursor.close();
+        cursor = newCursor;
+        if (newCursor != null) {
+            // Force the RecyclerView to refresh
+            this.notifyDataSetChanged();
+        }
+    }
+
+    //      1. DONE TODO  4pts The ability to mark a to do as done (a checkbox, or color in the list items for display)
+    // create object for checkbox
+    class ItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView descr;
+        TextView descr2;
+        TextView due;
+        CheckBox checkbox;
+        String duedate;
+        String description;
+//        String category;
+//        String check;
+        long id;
+
+
+//        item.xml checkbox is checkBox
+        ItemHolder(View view) {
+            super(view);
+            descr = (TextView) view.findViewById(R.id.description);
+
+//            inside item.xml variable and using the empty object CheckBox
+            checkbox = (CheckBox) view.findViewById(R.id.checkBox);
+            due = (TextView) view.findViewById(R.id.dueDate);
+            view.setOnClickListener(this);
+        }
+
+        public void bind(ItemHolder holder, int pos) {
+            cursor.moveToPosition(pos);
+            id = cursor.getLong(cursor.getColumnIndex(Contract.TABLE_TODO._ID));
+            Log.d(TAG, "deleting id: " + id);
+
+            duedate = cursor.getString(cursor.getColumnIndex(Contract.TABLE_TODO.COLUMN_NAME_DUE_DATE));
+            description = cursor.getString(cursor.getColumnIndex(Contract.TABLE_TODO.COLUMN_NAME_DESCRIPTION));
+
+//            category = cursor.getString(cursor.getColumnIndex(Contract.TABLE_TODO.COLUMN_NAME_CATEGORY));
+
+
+            descr.setText(description);
+            due.setText(duedate);
+            holder.itemView.setTag(id);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int pos = getAdapterPosition();
+            listener.onItemClick(pos, description, duedate, id);
+        }
+    }
+
+}
